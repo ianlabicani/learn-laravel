fe21145258f6a569c57e4757e6e031edd0140c97
ianlabicani <labicaniian@gmail.com>
2023-10-16 20:53:54 +0800

    create view for creating new product

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create</title>
+</head>
+
+<body>
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -10,3 +21,24 @@
     <form action="{{ route('product.store') }}" method="post">
         @csrf
         @method('post')
+        <div class="form-group">
+            <label>name</label>
+            <input type="text" name="name">
+        </div>
+        <div class="form-group">
+            <label>Quantity</label>
+            <input type="text" name="qty">
+        </div>
+        <div class="form-group">
+            <label>Price</label>
+            <input type="text" name="price">
+        </div>
+        <div class="form-group">
+            <label>Description</label>
+            <input type="text" name="description">
+        </div>
+        <input type="submit" value="Save">
+    </form>
+</body>
+
+</html>
