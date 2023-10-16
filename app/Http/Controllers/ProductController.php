a950424e174283d6ded7d4cf2a1961768829f553
ianlabicani <labicaniian@gmail.com>
2023-10-16 20:55:20 +0800

    create ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', ["products" => $products]);
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable',
+        ]);
+        Product::create($data);
+
+        return redirect(route('product.index'));
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', ['product' => $product]);
+    }
+
+    public function update(Product $product, Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable',
+        ]);
+        // dd($product);
+
+
+        $product->update($data);
+        return redirect(route('product.index'));
+    }
+}
